187156ed4ead8922a5fbe97ca6e398b8c2833889
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-03-06 10:36:31 -0500

    Set environment variables based on build variant

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,11 @@ if [[ "$target_platform" == linux-* ]]; then
     export CXX_FLAGS="$CXX_FLAGS -lGL -lGLU"
 fi
 
+if [[ "$build_variant" == "qt6" ]]; then
+    export WORKBENCH_USE_QT5=FALSE
+    export WORKBENCH_USE_QT6=TRUE
+fi
+
 cmake $CMAKE_ARGS -GNinja \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
